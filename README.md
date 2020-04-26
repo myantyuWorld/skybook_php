29a82da30a0add3c57a6737f11ee68285baa6d84
yuichi_develop <nepinepi984@gmail.com>
2020-04-26 10:48:29 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# 手順
+
+1. git clone XXX
+1. git clone https://github.com/abraham/twitteroauth.git
---
+++ twitter.php
@@ -0,0 +1,22 @@
+<?php
+require_once("twitteroauth/autoload.php");
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$consumerKey = "cw7nnZYUhD8emeuo2GVFcAeiW";
+$consumerSecret = "MeFHPZjqUYYRob2XODtB4tG3d6329Q7HyZw3NOf9B1IuAwB6xv";
+$accessToken = "1110529792720297984-K1lF3JuE7mrlitv0oRGxyNIxI1dcI9";
+$accessTokenSecret = "ePL3ExiQWkRCWbYVpdrGTvLpUNSn5a0CBGyZPm8UztbOr";
+  
+$twObj = new TwitterOAuth($consumerKey,$consumerSecret,$accessToken,$accessTokenSecret);
+$req = $twObj->OAuthRequest("https://api.twitter.com/1.1/lists/list.json","GET",array("count"=>"10"));
+$tw_arr = json_decode($req);
+ 
+if (isset($tw_arr)) {
+    foreach ($tw_arr as $key => $val) {
+        echo $tw_arr[$key]->text;
+        echo date('Y-m-d H:i:s', strtotime($tw_arr[$key]->created_at));
+        echo '<br>';
+    }
+} else {
+    echo 'つぶやきはありません。';
+}
